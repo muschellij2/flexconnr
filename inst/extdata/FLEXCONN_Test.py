8daaf4df625b5798ee327461595c79fe65065d16
muschellij2 <muschellij2@gmail.com>
2018-08-07 19:08:59 -0400

    Fixing the importing.

--- inst/extdata/FLEXCONN_Test.py
+++ inst/extdata/FLEXCONN_Test.py
@@ -14,6 +14,7 @@ from keras.models import load_model
 from keras import backend
 from scipy import ndimage
 from scipy.signal import argrelextrema
+from normalize_image import normalize_image
 
 
 backend.set_image_data_format = 'channels_last'
@@ -36,49 +37,6 @@ def apply_model(image1, image2, pred_model):
         output_image[:, :, k] = pred[0, :, :, 0]
 
     return output_image
-
-
-def normalize_image(vol, contrast):
-    temp = vol[np.nonzero(vol)].astype(float)
-    q = np.percentile(temp, 99)
-    temp = temp[temp <= q]
-    temp = temp.reshape(-1, 1)
-    bw = q / 80
-    print("99th quantile is %.4f, gridsize = %.4f" % (q, bw))
-
-    kde = sm.nonparametric.KDEUnivariate(temp)
-
-    kde.fit(kernel='gau', bw=bw, gridsize=80, fft=True)
-    x_mat = 100.0*kde.density
-    y_mat = kde.support
-
-    indx = argrelextrema(x_mat, np.greater)
-    indx = np.asarray(indx, dtype=int)
-    heights = x_mat[indx][0]
-    peaks = y_mat[indx][0]
-    peak = 0.00
-    print("%d peaks found." % (len(peaks)))
-    
-    # norm_vol = vol
-    if contrast.lower() == "t1":
-        peak = peaks[-1]
-        print("Peak found at %.4f for %s" % (peak, contrast))
-        # norm_vol = vol/peak
-        # norm_vol[norm_vol > 1.25] = 1.25
-        # norm_vol = norm_vol/1.25
-    elif contrast.lower() in ['t2', 'pd', 'fl']:
-        peak_height = np.amax(heights)
-        idx = np.where(heights == peak_height)
-        peak = peaks[idx]
-        print("Peak found at %.4f for %s" % (peak, contrast))
-        # norm_vol = vol / peak
-        # norm_vol[norm_vol > 3.5] = 3.5
-        # norm_vol = norm_vol / 3.5
-    else:
-        print("Contrast must be either T1,T2,PD, or FL. You entered %s. Returning 0." % contrast)
-    
-    # return peak, norm_vol
-    return peak
 
 
 def suffix(num):
